da1fb7aab6a6cef6b2226978402d1318ed20297c
Gregoire Detrez <detrez@chalmers.se>
2012-02-20 16:11:07 +0100

    Putting the return statement at the end

--- lab2/good/core101.cc
+++ lab2/good/core101.cc
@@ -1,25 +1,24 @@
 bool foo(int x) {
-    if (x == 0) {
-	return false;
-    } else {}
-
+  bool result ;
+  if (x == 0) {
+    result = false;
+  } else {
     bool b = true;
-
+    
     printInt(x);
-
-    return b;
+    result = b;
+  }
+  return result;
 }
 
 void printBool(bool b) {
-	if (false) {}
+  if (false) {}
   else
-
-  if (b) {
-	  printInt(0);
-  } else {
-	  printInt(1);
-  }
-
+    if (b) {
+      printInt(0);
+    } else {
+      printInt(1);
+    }
 }
 
 int main() {
